4f62b05381fbcc74e1e3b049c49e59b71708bd18
Himanshu Yadav <himanshuyadav@Himanshus-MacBook-Air.local>
2025-08-31 14:28:54 +0530

    Initial commit: Swindler Dating App ready for deployment

---
+++ swindler-deployment/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'swindler-v1';
+const urlsToCache = [
+  '/',
+  '/hinge-mvp.html',
+  '/login-demo.html',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached response if found
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
